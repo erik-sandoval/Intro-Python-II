ec9ceac8a407bc2244ff676eb8e5bb5dcbc8996d
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-04 22:06:38 -0700

    added back directions

--- src/room.py
+++ src/room.py
@@ -3,14 +3,15 @@
 
 
 class Room():
-    def __init__(self, name, descrip):
+    def __init__(self, name, descrip, items):
         # n_to, s_to, e_to, w_to
         self.name = name
         self.descrip = descrip
-        # self.n_to = n_to
-        # self.s_to = s_to
-        # self.e_to = e_to
-        # self.w_to = w_to
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.items = items
 
     def __repr__(self):
         return f"name: {self.name}, description: {self.descrip}"
