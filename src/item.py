6080b7e9cf5c31f89b81a84c08953060b77f81f5
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-04 22:04:45 -0700

    added item describe.

--- src/item.py
+++ src/item.py
@@ -3,3 +3,6 @@ class Item():
     def __init__(self, name, descr):
         self.name = name
         self.descr = descr
+
+    def __repr__(self):
+        return f"item: {self.name}, description: {self.descr}"
