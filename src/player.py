cf12b817fbdba9b4be917d39786b5dc01e43b808
Erik Sandoval <erik.sandoval@icloud.com>
2019-09-04 22:06:18 -0700

    added item inventory to player class

--- src/player.py
+++ src/player.py
@@ -6,10 +6,10 @@ class Player():
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.items = []
 
-    
+    def addItem(self, item):
+        return self.items.append(item)
 
     def __repr__(self):
-        return f"name: {self.name}, current_room: {self.current_room}"
-
-
+        return f"name: {self.name}, current_room: {self.current_room},"
